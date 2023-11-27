70d28d2fdf4fef3f03ec798be9d8509c56e04195
Andrey Frolov <senior.slender2015@gmail.com>
2023-11-27 15:13:41 +0200

    GSAP Plugin Registration

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 window.addEventListener('scroll', e => {
     document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+    
 });
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
